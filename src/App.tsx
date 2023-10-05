79b886942748ac382b2aa0184634670a3881eb1e
zelimdemilh <zelemhanzzz@mail.ru>
2023-10-05 19:49:40 +0300

    добавляем темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import "./styles/index.scss"
-import { Suspense } from "react"
+import "./styles/index.scss";
+import { Suspense } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
-        <Link to="/">Main</Link>
-        <Link to="/about">About</Link>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/about" element={<AboutPageAsync />}/>
-            <Route path="/" element={<MainPageAsync />}/>
-          </Routes>
-        </Suspense>
-       
+    <div className={`app ${theme}`}>
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <button onClick={() => toggleTheme()}>Toggle</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
-  )
+  );
 };
 
 export default App;
